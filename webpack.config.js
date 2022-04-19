403652d528f1f922447caf67a70b66f9871f327f
jongwooo <han980817@gmail.com>
2022-04-20 01:01:52 +0900

    style: Update webpack.config.js to format code

--- webpack.config.js
+++ webpack.config.js
@@ -16,9 +16,7 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                include: [
-                    path.resolve(__dirname, 'src/js')
-                ],
+                include: path.resolve(__dirname, 'src/js'),
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
