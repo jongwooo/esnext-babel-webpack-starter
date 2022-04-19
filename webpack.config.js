b57369fadedb90029886b8729e327bb35b1a843a
jongwooo <han980817@gmail.com>
2022-04-20 01:04:37 +0900

    style: Remove unnecessary comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,11 @@
 const path = require('path');
 
 module.exports = {
-    // entry file
-    // https://webpack.js.org/configuration/entry-context/#entry
     entry: ['@babel/polyfill', './src/js/main.js'],
-    // 번들링된 js 파일의 이름(filename)과 저장될 경로(path)를 지정
-    // https://webpack.js.org/configuration/output/#outputpath
-    // https://webpack.js.org/configuration/output/#outputfilename
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/bundle.js'
     },
-    // https://webpack.js.org/configuration/module
     module: {
         rules: [
             {
@@ -29,6 +23,5 @@ module.exports = {
         ]
     },
     devtool: 'source-map',
-    // https://webpack.js.org/configuration/mode
     mode: 'development'
 };
